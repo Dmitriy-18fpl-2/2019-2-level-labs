ff0730b9977722bd5060d67a0590aa05379d4375
Dmitriy-18fpl-2 <55382491+Dmitriy-18fpl-2@users.noreply.github.com>
2019-09-16 11:45:31 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -9,6 +9,7 @@ def calculate_frequences(text: str) -> dict:
     Calculates number of times each word appears in the text
     """
     pass
+x=5
 
 def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
     """
