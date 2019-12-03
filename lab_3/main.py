61e0a3ba9c928fe89fca6f2c33cd3f0d622adc7c
Dmitriy-18fpl-2 <balyasnikov.mitya@yandex.ru>
2019-12-03 09:19:02 +0300

    Added extrafunc just in case, some lint errors fixed

--- lab_3/main.py
+++ lab_3/main.py
@@ -16,31 +16,31 @@ class WordStorage:
         self.storage = {}
 
     def put(self, word: str) -> int:
-        if word not in self.storage and type(word) == str:
+        if word not in self.storage and isinstance(word, str):
             word_id = len(list(self.storage.keys())) + 1
             self.storage[word] = word_id
             return word_id
         else:
             print("ERROR")
-            return False
 
     def get_id_of(self, word: str) -> int:
-        if word in self.storage and type(word) == str:
+        if word in self.storage and isinstance(word, str):
             return self.storage[word]
         else:
-            return (-1)
+            return -1
 
     def get_original_by(self, id: int) -> str:
+        word_id = id
         list_of_keys = list(self.storage.keys())
         list_of_values = list(self.storage.values())
-        if id in list_of_values and type(id) == int:
-            return list_of_keys[list_of_values.index(id)]
+        if word_id in list_of_values and isinstance(word_id, int):
+            return list_of_keys[list_of_values.index(word_id)]
         else:
             return "UNK"
 
     def from_corpus(self, corpus: tuple):
         id_counter = 1
-        if type(corpus) == tuple and corpus != () and type(corpus[0]) == list:
+        if isinstance(corpus, tuple) and corpus != () and isinstance(corpus[0], list):
             for line in range(len(corpus)):
                 for word in range(len(corpus[line])):
                     if corpus[line][word] not in self.storage:
@@ -48,7 +48,7 @@ class WordStorage:
                         id_counter += 1
                     else:
                         continue
-        elif type(corpus) == tuple and corpus != ():
+        elif isinstance(corpus, tuple) and corpus != ():
             for word in range(len(corpus)):
                 if corpus[word] not in self.storage:
                     self.storage[corpus[word]] = id_counter
@@ -69,7 +69,7 @@ class NGramTrie:
         self.gram_log_probabilities = {}
 
     def fill_from_sentence(self, sentence: tuple) -> str:
-        if type(sentence) == tuple and sentence != ():
+        if isinstance(sentence, tuple) and sentence != ():
             if len(sentence) <= self.size:
                 last_word = sentence[len(sentence) - 1]
                 sentence = list(sentence)
@@ -87,6 +87,7 @@ class NGramTrie:
                         self.gram_frequencies[gram] = 1
                     else:
                         self.gram_frequencies[gram] += 1
+            return "OK"
         else:
             return "ERROR"
 
@@ -102,7 +103,7 @@ class NGramTrie:
             return False
 
     def predict_next_sentence(self, prefix: tuple) -> list:
-        if type(prefix) == tuple and len(prefix) == self.size - 1:
+        if isinstance(prefix, tuple) and len(prefix) == self.size - 1:
             predicted_sentence = list(prefix)
             support_number = 0
             while True:
@@ -137,7 +138,7 @@ def encode(storage_instance, corpus) -> list:
     for line_index in range(len(corpus)):
         encoded_line = []
         for index, word in enumerate(corpus[line_index]):
-            encoded_line.insert(index, storage_instance.get_id_of[word])
+            encoded_line.insert(index, storage_instance.get_id_of(word))
         encoded_corpus.insert(line_index, encoded_line)
     return encoded_corpus
 
@@ -146,7 +147,7 @@ def split_by_sentence(text: str) -> list:
     test_objects = "qwertyuioplkjhgfdsazxcvbnm. "
     signs = "!?"
     text_clean = ""
-    if type(text) == str and text != '' and "." in text:
+    if isinstance(text, str) and text != '' and "." in text:
         text_low = text.lower()
         for i in range(len(text_low)):
             if text_low[i] in test_objects:
@@ -167,4 +168,19 @@ def split_by_sentence(text: str) -> list:
         print('ERROR')
         return []
 
+
+def all_in_one(text, n, prefix):
+    test_storage = WordStorage()
+    sample = tuple(split_by_sentence(text))
+    test_storage.from_corpus(sample)
+    encoded_corp = encode(test_storage, sample)
+    sample_gram = NGramTrie(n)
+    for sentence in encoded_corp:
+        sample_gram.fill_from_sentence(tuple(sentence))
+    sample_gram.calculate_log_probabilities()
+    prefix_list = []
+    for word in prefix:
+        prefix_list.insert(len(prefix_list), test_storage.get_id_of(word))
+    prefix = tuple(prefix_list)
+    return sample_gram.predict_next_sentence(prefix)
 
